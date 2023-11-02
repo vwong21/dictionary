59f8e5965bb4b0676ab78630255d92019d8c1201
Vincent Wong <vincentwong5609@gmail.com>
2023-11-02 00:00:17 -0700

    Style header

--- src/header.jsx
+++ src/header.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { useState } from "react";
 import ReactSwitch from 'react-switch'
-import {BiMoon} from 'react-icons/bi'
+import {PiMoonLight} from 'react-icons/pi'
+import {PiBookThin} from 'react-icons/pi'
+import './header.css'
 
 const Header = ({theme, toggleTheme}) => {
 
     return (
-        <section id="header">
-            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} checkedIcon/>
-            <BiMoon/>
-        </section>
+        <header>
+            <div id="left-header">
+                <PiBookThin id="book"/>
+            </div>
+            
+            <div id="right-header">
+                <div id="switch-container">
+                    <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} checkedIcon onColor="#A147E9" uncheckedIcon={false}/>
+                    <PiMoonLight id="moon"/> 
+                </div>
+            </div>
+            
+            
+        </header>
     )
 }
 
